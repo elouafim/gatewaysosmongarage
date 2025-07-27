ccac3af53c271a0975681c7b90d0d7d4720d5361
elouafim <mohammedelouafi02@icloud.com>
2025-07-27 14:42:58 +0100

    first commit

---
+++ src/main/java/org/example/gatewaysosmongarage/GatewaysosmongarageApplication.java
@@ -0,0 +1,13 @@
+package org.example.gatewaysosmongarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaysosmongarageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewaysosmongarageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gatewaysosmongarage/GatewaysosmongarageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gatewaysosmongarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaysosmongarageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
